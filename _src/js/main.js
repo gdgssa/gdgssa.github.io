6d1604189543b791b3b9bf1695cc827838385f7c
mvfsilva <marcusvinicius30798@gmail.com>
2016-02-11 01:50:54 -0300

    Adicioado logo em animação em js

--- _src/js/main.js
+++ _src/js/main.js
@@ -9,12 +9,14 @@
           pageSize = (doc.clientHeight - window.innerHeight),
           percentageScrolled = Math.floor((scrollPosition / pageSize) * 100); 
           
-          if (percentageScrolled > 65){
+          if (percentageScrolled > 50){
             document.querySelector('.transparent').className = 'blue';
+            document.querySelector('.new-logo').style.display = '';
           } 
 
           else{
             document.querySelector('.blue').className = 'transparent';
+            document.querySelector('.new-logo').style.display = 'none';
           }
 
         }; 
