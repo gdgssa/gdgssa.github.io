c0d527acbc329e2fc80517b542c0da3f3768cb02
mvfsilva <marcusvinicius30798@gmail.com>
2016-03-07 10:33:47 -0300

    Retirando console log do código

--- _src/js/main.js
+++ _src/js/main.js
@@ -9,7 +9,6 @@
           pageSize = (doc.clientHeight - window.innerHeight),
           percentageScrolled = Math.floor((scrollPosition / pageSize) * 100); 
           
-          console.log('Scroll: ', percentageScrolled);
           if (percentageScrolled > 20){
             document.querySelector('.transparent').className = 'teal';
             document.querySelector('.new-logo').style.display = '';
