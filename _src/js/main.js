de673e7b0d562b3651df0b320a66f9d4d8df887b
mvfsilva <marcusvinicius30798@gmail.com>
2016-03-23 00:52:39 -0300

    Refatorando main.js

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,28 +1,22 @@
-(function(){
+(function(win, doc){
   'use strict';
   
-    window.onload = function () { 
-      window.scrollTo(0, 0); 
-      window.onscroll = function () { 
-        var doc = document.body; 
-        var scrollPosition = doc.scrollTop;
-        var pageSize = (doc.clientHeight - window.innerHeight);
-        var percentageScrolled = Math.floor((scrollPosition / pageSize) * 100); 
-        
-        var bgColor = $('#bgColor');
-        var gdgLogo = $('#logo-container');
-        var colorHeader = $('.teal');
-
-        gdgLogo.hide();
-        
-        if (percentageScrolled < 20){
-          colorHeader.addClass('transparent');
-        } else {
-          bgColor.removeClass('transparent');
-          bgColor.addClass('teal');
-          gdgLogo.show();
+    onload = function () { 
+      scrollTo(0, 0); 
+      onscroll = function () { 
+        var $bgColor = $('#bgColor');
+        var $gdgLogo = $('#logo-container');
+        var $colorHeader = $('.teal');
+        var pageSize = (doc.body.clientHeight - win.innerHeight);
+        var percentageScrolled = Math.floor((doc.body.scrollTop / pageSize) * 100); 
+                
+        if (percentageScrolled < 20)
+          $colorHeader.addClass('transparent');
+        else {
+          $bgColor.removeClass('transparent');
+          $bgColor.addClass('teal');
+          $gdgLogo.show();
         }
-
       }; 
     };
-})();
+})(window, document);
